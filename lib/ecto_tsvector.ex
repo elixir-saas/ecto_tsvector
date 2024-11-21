749bc6be3d140258999960672988afe878176fe1
Justin Tormey <jrtormey@gmail.com>
2024-11-21 13:07:29 -0600

    Initial commit

---
+++ lib/ecto_tsvector.ex
@@ -0,0 +1,18 @@
+defmodule EctoTsvector do
+  @moduledoc """
+  Documentation for `EctoTsvector`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoTsvector.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_tsvector_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoTsvectorTest do
+  use ExUnit.Case
+  doctest EctoTsvector
+
+  test "greets the world" do
+    assert EctoTsvector.hello() == :world
+  end
+end
